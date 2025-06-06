bb13bdd4b4ad79cb707deb969686c96a78ff1e62
priyanshu <priyanshu.23bora@gmail.com>
2025-06-06 13:39:28 +0530

    second-commit

--- client/src/store/movieStore.jsx
+++
@@ -1,9 +0,0 @@
-// import { BACKEND_URL } from "../config.js";
-// import axios from "axios";
-// import { create } from "zustand";
-// import toast from "react-hot-toast";
-
-
-// export const useMovieStore = create ((set) => ({
-
-// }))
